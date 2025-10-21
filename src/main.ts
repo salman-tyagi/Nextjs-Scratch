14d936c828a0459dc81cbbfa68ac20b683ec5c83
salman-tyagi <salmantyagi15@gmail.com>
2025-10-21 16:28:34 +0530

    initialized get request

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { Controller, Module, Get } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
 
 @Controller()
 class AppController {
@@ -7,3 +8,18 @@ class AppController {
     return 'Hi there!';
   }
 }
+
+@Module({
+  controllers: [AppController],
+})
+class AppModule {}
+
+(async () => {
+  const app = await NestFactory.create(AppModule);
+
+  const PORT = 8000;
+
+  app.listen(PORT, () => {
+    console.log(`Listening on the port ${PORT}`);
+  });
+})();
